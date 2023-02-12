3a3c95ee70443f68f5c8954daeec328b0d292f23
ToskePG <danilo.tosic.pmf@gmail.com>
2023-02-13 00:59:35 +0100

    Creates NotesViewModel. Creates NotesState and NotesEvent.

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,5 +1,7 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_cases.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,5 +11,24 @@ import javax.inject.Inject
 class NotesViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ) : ViewModel() {
-    
+
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
+
+    fun onEvent(event: NotesEvent){
+        when(event){
+            is NotesEvent.Order -> {
+
+            }
+            is NotesEvent.DeleteNote -> {
+
+            }
+            is NotesEvent.RestoreNote -> {
+
+            }
+            is NotesEvent.ToggleOrderSection -> {
+
+            }
+        }
+    }
 }
